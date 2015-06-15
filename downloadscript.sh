10dbc7c0d3b7b1df1d3fa007d63f0b0c9f214def
Gloria Silveira <gloriasilveira@users.noreply.github.com>
2015-06-15 16:39:14 -0700

    Script which tells Linux what to download

---
+++ downloadscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script that states which URLs to download from
+
+wget https://raw.githubusercontent.com/gloriasilveira/blog_posts/master/anothertest.md
+wget https://raw.githubusercontent.com/gloriasilveira/blog_posts/master/testpost2.md
+wget https://raw.githubusercontent.com/gloriasilveira/blog_posts/master/yetanothertest.md
